e4d50bb8af5c466977a11556ee707cf8597ed2d4
com.xin <kleine4augen@gmail.com>
2024-03-10 20:21:12 +0100

    init

---
+++ starter/src/component/Book.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({book, shelf, onChangeShelf}) => {
+    return (
+        <li key={book.id}>
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${book.imageLinks.thumbnail})`,
+                        }}
+                    ></div>
+                    <div className="book-shelf-changer">
+                        <select
+                            defaultValue={shelf}
+                            onChange={(e) => onChangeShelf(book, e.target.value)}
+                        >
+                            <option value="none" disabled>
+                                {book.shelf ? "Move to..." : "Add to ..."}
+                            </option>
+                            <option value="currentlyReading">
+                                {shelf === "currentlyReading" && "✔ "}Currently Reading
+                            </option>
+                            <option value="wantToRead">
+                                {shelf === "wantToRead" && "✔ "}Want to Read
+                            </option>
+                            <option value="read">
+                                {shelf === "read" && "✔ "}Read
+                            </option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                    {book.authors.join(', ')}
+                </div>
+            </div>
+        </li>
+    );
+};
+
+Book.propTypes = {
+    book: PropTypes.object.isRequired,
+    shelf: PropTypes.string.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+};
+
+export default Book;
---
+++ starter/src/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import {createRoot} from 'react-dom/client';
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
+
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
